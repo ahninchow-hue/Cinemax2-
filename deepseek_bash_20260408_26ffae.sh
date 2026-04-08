e58735d470de42736c43e5e1e82b2d1cfe70fb63
ahninchow-hue <ahninchow@gmail.com>
2026-04-08 04:31:25 +0100

    Add files via upload

---
+++ deepseek_bash_20260408_26ffae.sh
@@ -0,0 +1,6 @@
+git init
+git add .
+git commit -m "النسخة الأولى من موقع سينما ناو"
+git branch -M main
+git remote add origin https://github.com/اسم-المستخدم/اسم-المستودع.git
+git push -u origin main
